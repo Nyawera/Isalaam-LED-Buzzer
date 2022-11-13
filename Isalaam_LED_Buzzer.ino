71dee139e5130102db6c43ab29529beb38cc1c70
Nyawera <99425463+Nyawera@users.noreply.github.com>
2022-11-13 19:59:59 +0300

    Add files via upload

---
+++ Isalaam_LED_Buzzer.ino
@@ -0,0 +1,21 @@
+  void setup()
+{
+    pinMode(9, OUTPUT);
+
+      pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(9, LOW);
+    delay(1000);  // wait for 1000 millisecond (s)
+    
+    digitalWrite(9, HIGH);
+    delay(500); // wait for 1000 millisecond (s)
+
+
+    digitalWrite(7, HIGH);
+  delay(5000); // Wait for 5000 millisecond(s)
+  digitalWrite(7, HIGH);
+  delay(5000); // Wait for 5000 millisecond(s)
+}
